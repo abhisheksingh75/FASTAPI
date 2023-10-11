ec63afcc9457f43ab05a17507129ae584d92d8a2
Abhishek <abhisheksinghpnp@gmail.com>
2023-10-11 16:07:38 +0530

    fast api data types

--- chapter1.py
+++ chapter1.py
@@ -10,6 +10,7 @@ tags_metadata = [
 app = FastAPI(openapi_tags=tags_metadata)
 
 
+#Here, @app.get("/") is a decorator that specifies that this function should handle HTTP GET requests at the root path ("/").
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI! :)"}
