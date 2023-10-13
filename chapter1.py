f41fd7263765ed3b7c7896825dc0ab35b5e942a1
Abhishek <abhisheksinghpnp@gmail.com>
2023-10-13 14:52:07 +0530

    updated chapter 1 tittle

--- chapter1.py
+++ chapter1.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 tags_metadata = [
     {
-        "name": "Chapter 1",
-        "description": "Creating your first FastAPI Server",
+        "name": "Chapter 1 - Creating your first FastAPI Server",
     }
 ]
 
