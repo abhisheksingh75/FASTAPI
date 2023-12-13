5ccaf72d1966561692c28426dc5b68811eb3df45
Abhishek <abhisheksinghpnp@gmail.com>
2023-12-13 09:56:38 +0530

    cart operations

--- cookies_operation.py
+++ cookies_operation.py
@@ -1,26 +1,54 @@
 from fastapi import FastAPI, Response, Request
+from typing import List, Dict
+import json
+from fastapi import HTTPException
 
 tags_metadata = [
     {
-        "name": "cookies operation",
+        "name": "cart operations",
+        "description": "Operations to manage an e-commerce cart using cookies."
     }
 ]
 
 app = FastAPI(openapi_tags=tags_metadata)
 
-@app.post("/set_cookie")
-def set_cookie(response: Response):
-    response.set_cookie(key="mycookie", value="cookie_value")
-    return {"message": "Cookie set"}
+# A simple representation of a product
+class Product:
+    def __init__(self, id: str, name: str, price: float):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "price": self.price}
+
+# Sample product inventory
+product_inventory = {
+    "p1": Product("p1", "Laptop", 999.99),
+    "p2": Product("p2", "Smartphone", 499.99),
+    "p3": Product("p3", "Headphones", 89.99)
+}
 
 
-@app.get("/get_cookie")
-def get_cookie(request: Request):
-    cookie_value = request.cookies.get("mycookie")
-    return {"cookie_value": cookie_value}
+
+@app.post("/add_to_cart")
+def add_to_cart(response: Response, product_id: str, request: Request):
+    product = product_inventory.get(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found in inventory")
+    cart = json.loads(request.cookies.get("cart", "[]"))
+    cart.append(product.to_dict())
+    response.set_cookie(key="cart", value=json.dumps(cart))
+    return {"message": f"Added {product.name} to cart."}
 
 
-@app.delete("/delete_cookie")
-def delete_cookie(response: Response):
-    response.delete_cookie(key="mycookie")
-    return {"message": "Cookie deleted"}
+@app.get("/view_cart")
+def view_cart(request: Request):
+    cart = json.loads(request.cookies.get("cart", "[]"))
+    return {"cart": cart}
+
+@app.delete("/clear_cart")
+def clear_cart(response: Response):
+    response.delete_cookie(key="cart")
+    return {"message": "Cart cleared"}
+
