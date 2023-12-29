dbdd27355176ffb377f8bf588617bffbb340a097
Abhishek <abhisheksinghpnp@gmail.com>
2023-12-29 15:50:10 +0530

    connection url

--- nosql_db_example/config/database.py
+++ nosql_db_example/config/database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb")
+client = MongoClient("<mongodb_connection_url>")
 
 db = client.books_database
 collection_name = db["books_collection"]
