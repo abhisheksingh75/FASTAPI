e7c69d30831999f1e4b1b31edd7e4ae75ab72f12
Abhishek <abhisheksinghpnp@gmail.com>
2024-01-06 22:25:53 +0530

    static files

---
+++ static_files/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# Mounting static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse("static/index.html")
